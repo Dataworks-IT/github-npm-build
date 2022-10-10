c063c951c15504fa44ef4b91d2cc9174de5e4b41
erikrob <erikrob@gmail.com>
2022-10-10 14:59:20 +0200

    forward variable values

--- entrypoint.sh
+++ entrypoint.sh
@@ -51,7 +51,7 @@ then
   fi
 else
   mkdir $HOME/.aws && \
-  cat <<EOF >$HOME/.aws/credentials
+  PROFILE=$PROFILE AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY cat <<EOF >$HOME/.aws/credentials
 [$PROFILE]
 aws_access_key_id = $AWS_ACCESS_KEY_ID
 aws_secret_access_key = $AWS_SECRET_ACCESS_KEY
